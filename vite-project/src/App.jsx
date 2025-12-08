884fe49c0abbf4f5d600c9342320218317797a51
Mikołaj Michalak <majkanart117@gmail.com>
2025-12-08 13:37:24 +0100

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Twoja nowa aplikacja</h1>
+      <p>Start od zera — dodaj swoje komponenty tutaj1.</p>
+    </div>
+  )
+}
