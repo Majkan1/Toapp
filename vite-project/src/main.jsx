537688b8791624d7f5a73a33b46276e0a582ed48
Mikołaj Michalak <majkanart117@gmail.com>
2025-12-08 19:04:54 +0100

    Use TodoApp as entry; clean comment

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-{/*import App from './App.jsx'*/}
+// import App from './App.jsx'
 import TodoApp from './Todo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TodoApp/>
+    <TodoApp />
   </StrictMode>
 )
