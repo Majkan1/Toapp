39a013b565ec075c48f913df278cb602bbb0bc12
Mikołaj Michalak <majkanart117@gmail.com>
2025-12-08 13:42:36 +0100

    Configure GH Pages for Toapp

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Ustaw `base` na `/<twoje-repo>/` aby zasoby działały na GitHub Pages
-  base: '/<twoje-repo>/',
+  // Base dla GitHub Pages
+  base: '/Toapp/',
   plugins: [react()],
 })
