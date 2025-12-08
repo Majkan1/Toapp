7d42258bee8bca100d619ae51b6e8f0861ac8651
Mikołaj Michalak <majkanart117@gmail.com>
2025-12-08 19:10:58 +0100

    Remove App.jsx; use TodoApp only

--- vite-project/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Twoja nowa aplikacja</h1>
-      <p>Start od zera — dodaj swoje komponenty tutaj1.</p>
-    </div>
-  )
-}
